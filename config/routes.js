3618e3d5857735ce26c398a6938576bf6920925a
Robert Cabrera <cabrera.rch@gmail.com>
2022-09-17 12:10:14 +0200

    [FEATURE] Create all Income's CRUD, tested in Postman and verified in MongoDB

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const incomesController = require("../controllers/incomes.controller");
 
+/* HOME */
 router.get("/", (req, res, next) => res.status(200).json({ ok: true }));
+
+/* INCOMES */
 router.post("/incomes/new", incomesController.create);
+router.get("/income/:id", incomesController.detail);
+router.patch("/income/:id", incomesController.update);
+router.delete("/income/:id", incomesController.delete);
 
 module.exports = router;
--- controllers/incomes.controller.js
+++ controllers/incomes.controller.js
@@ -2,9 +2,26 @@ const Income = require("../models/Income.model");
 
 module.exports.create = (req, res, next) => {
   const income = ({ title, amount, category } = req.body);
-  console.log(income);
 
   Income.create(income)
     .then((newIncome) => res.status(200).json(newIncome))
+    .catch(next);
+};
+
+module.exports.detail = (req, res, next) => {
+  Income.findById(req.params.id)
+    .then((income) => res.status(200).json(income))
+    .catch(next);
+};
+
+module.exports.update = (req, res, next) => {
+  Income.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedIncome) => res.status(200).json(updatedIncome))
+    .catch(next);
+};
+
+module.exports.delete = (req, res, next) => {
+  Income.findByIdAndRemove(req.params.id)
+    .then((incomeToDelete) => res.status(200).json(incomeToDelete))
     .catch(next);
 };
